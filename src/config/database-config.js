6137b2ba362d1903cd8d733aa75d41342c09d7ce
Alison <alisonmozer@gmail.com>
2023-06-16 11:00:50 -0300

    render

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,14 +1,14 @@
 
 // Configuração do bando de dados no ambiente de teste
-export const dbConfig = {
-  dialect: 'sqlite',
-  storage: 'database.sqlite',
-  define: {
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true
-  }
-};
+// export const dbConfig = {
+//   dialect: 'sqlite',
+//   storage: 'database.sqlite',
+//   define: {
+//     timestamps: true,
+//     freezeTableName: true,
+//     underscored: true
+//   }
+// };
 
 /*
 // Configuração do banco de dados no ambiente de desenvolvimento
@@ -26,18 +26,19 @@ export const dbConfig = {
 };
 */
 
-/*
 // Configuração do banco de dados no ambiente de produção
-export const databaseConfig = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'scv-backend-node-sequelize',
+export const dbConfig = {
+  dialect: "postgres",
+  host: "dpg-ci660v5ph6ekv7sl170g-a.oregon-postgres.render.com",
+  username: "sclv_lava_jato_user",
+  password: "AyX47JhdSq5MObqoNGreG2Ydgw7Tedir",
+  database: "sclv_lava_jato",
   define: {
     timestamps: true,
     freezeTableName: true,
-    underscored: true
-  }
+    underscored: true,
+  },
+  dialectOptions: {
+    ssl: true,
+  },
 };
-*/
